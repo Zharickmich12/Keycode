6a8cb8cb40f23a60138bdcedc14f52b9abedde2f
Zharick Michelle <128750315+Zharickmich12@users.noreply.github.com>
2025-09-29 23:42:23 -0500

    -

--- app-nest/src/app.service.ts
+++ app-nest/src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getStatus(): { status: string, time: Date | string }{
+    return { status: 'ok', time: new Date().toISOString() };
+  }
 }
