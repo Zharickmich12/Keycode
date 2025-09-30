5c0d45ac901d79eb6ce50ff54d1a6d5ffad621ea
Zharick Michelle <128750315+Zharickmich12@users.noreply.github.com>
2025-09-30 16:56:52 -0500

    nest

---
+++ app-nest/src/interfaces/IProducts.ts
@@ -0,0 +1,1 @@
+export type IProducts = { id: number, name: string, description: string, price: number }
--- app-nest/src/modules/users/users.service.ts
+++ app-nest/src/modules/users/users.service.ts
@@ -1,13 +1,12 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-
-export type IUser = { id: number, name: string, email: string}
+import { IUser } from 'src/interfaces';
 
 @Injectable()
 export class UsersService {
     
     private users: IUser[] = [
-        { id: 1, name: 'Elena', email: 'elena@gmail.com'},
-        { id: 2, name: 'Jefferson', email: 'pulido@gmail.com'}
+        { id: 1, name: 'Elena', email: 'elena@gmail.com', password: '123' },
+        { id: 2, name: 'Jefferson', email: 'pulido@gmail.com', password: 'asd123', age: 23 }
     ]
 
     findAll(): IUser[] {
